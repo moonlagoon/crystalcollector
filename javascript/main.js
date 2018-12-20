7290408843979196490077ec95ec6fe7380f126b
moonlagoon <courtneyroutsong@gmail.com>
2018-12-20 18:44:12 -0500

    kitty collector

---
+++ javascript/main.js
@@ -0,0 +1,72 @@
+
+
+var targetNumber = "";
+var wins = 0;
+var losses = 0;
+var counter = 0;
+
+var images = ["cat1.png", "cat2.png", "cat3.png", "cat4.png"];
+
+
+
+        function randomtargetNumber () {
+		targetNumber = Math.floor(Math.random() * 100) + 18;
+    }
+    
+
+
+    
+
+	function resetCats () {
+		for (var i = 0; i < images.length; i++) {
+			var kittyCat = $("<img>");
+			kittyCat.addClass("kittyCat");
+			kittyCat.attr("src", images[i]);
+			kittyCat.attr("value", (Math.floor(Math.random() * 12) + 1));
+			kittyCat.attr("height", "100");
+			$(".kittyImages").append(kittyCat);
+		}
+	}
+
+
+
+
+	function resetHTML () {
+		$(".target-number").html(targetNumber);
+		$(".winLose").html("<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>");
+		$(".scoreBoy").html(counter);
+		$(".kittyImages").empty();
+	}
+
+	function bigBetReset () {
+		randomtargetNumber ();
+		counter = 0;
+		resetHTML ();
+		resetCats ();
+	}
+
+
+	randomtargetNumber();
+	resetHTML ();
+	resetCats ();
+
+
+
+
+                                                   //onclick
+	function kittyCatClick () {
+
+		counter += parseInt($(this).attr("value"));
+		$(".scoreBoy").html(counter);
+		if (counter == targetNumber) {
+			wins++;
+			bigBetReset();
+		}
+		else if (counter > targetNumber) {
+			losses++;
+			bigBetReset();
+		};
+	};
+
+	
+	$(document).on("click", ".kittyCat", kittyCatClick);
